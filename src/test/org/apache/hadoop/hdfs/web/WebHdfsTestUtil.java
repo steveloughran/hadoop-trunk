3504361a8e77e4eea747fdbd4ff041f932515f5e
Tsz-wo Sze <szetszwo@apache.org>
2012-06-26 10:49:22 +0000

    HDFS-3516. Check content-type in WebHdfsFileSystem.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/common/branches/branch-1@1353928 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/hadoop/hdfs/web/WebHdfsTestUtil.java
+++ src/test/org/apache/hadoop/hdfs/web/WebHdfsTestUtil.java
@@ -55,7 +55,7 @@ public class WebHdfsTestUtil {
 
   public static WebHdfsFileSystem getWebHdfsFileSystemAs(
       final UserGroupInformation ugi, final Configuration conf
-      ) throws IOException, URISyntaxException, InterruptedException {
+      ) throws IOException, InterruptedException {
     return ugi.doAs(new PrivilegedExceptionAction<WebHdfsFileSystem>() {
       @Override
       public WebHdfsFileSystem run() throws Exception {
@@ -76,7 +76,7 @@ public class WebHdfsTestUtil {
       final int expectedResponseCode) throws IOException {
     conn.connect();
     Assert.assertEquals(expectedResponseCode, conn.getResponseCode());
-    return WebHdfsFileSystem.jsonParse(conn.getInputStream());
+    return WebHdfsFileSystem.jsonParse(conn, false);
   }
   
   public static HttpURLConnection twoStepWrite(HttpURLConnection conn,
