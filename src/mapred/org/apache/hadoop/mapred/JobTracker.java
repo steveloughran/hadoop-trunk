ac41cf8edfb869af16681d370066bc2a71e1f0ca
Suresh Srinivas <sureshms@yahoo-inc.com>
2012-07-27 09:50:18 -0700

    RMP-270. MAPREDUCE-1906. Lower minimum heartbeat interval between tasktracker and jobtracker for smaller clusters. Backport form branch-1

--- src/mapred/org/apache/hadoop/mapred/JobTracker.java
+++ src/mapred/org/apache/hadoop/mapred/JobTracker.java
@@ -3198,7 +3198,7 @@ public class JobTracker implements MRConstants, InterTrackerProtocol,
     int clusterSize = getClusterStatus().getTaskTrackers();
     int heartbeatInterval =  Math.max(
                                 (int)(1000 * HEARTBEATS_SCALING_FACTOR *
-                                      Math.ceil((double)clusterSize / 
+                                      ((double)clusterSize / 
                                                 NUM_HEARTBEATS_IN_SECOND)),
                                 HEARTBEAT_INTERVAL_MIN) ;
     return heartbeatInterval;
