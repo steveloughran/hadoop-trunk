f72f40844792b79e6e1b0eb502f44dcb66e648a4
Steve Loughran <stevel@hortonworks.com>
2013-05-23 20:24:12 +0100

    add constructor to YarnClientImpl to allow subclasses to provide the service name

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-client/src/main/java/org/apache/hadoop/yarn/client/YarnClientImpl.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-client/src/main/java/org/apache/hadoop/yarn/client/YarnClientImpl.java
@@ -79,7 +79,11 @@ public class YarnClientImpl extends AbstractService implements YarnClient {
   }
   
   public YarnClientImpl(InetSocketAddress rmAddress) {
-    super(YarnClientImpl.class.getName());
+    this(YarnClientImpl.class.getName(), rmAddress);
+  }
+
+  public YarnClientImpl(String name, InetSocketAddress rmAddress) {
+    super(name);
     this.rmAddress = rmAddress;
   }
 
