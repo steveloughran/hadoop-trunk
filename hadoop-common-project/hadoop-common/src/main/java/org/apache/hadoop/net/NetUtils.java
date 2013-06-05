ad295a70e6619d9d2f8661ce88629b5af088bf78
Steve Loughran <stevel@hortonworks.com>
2013-06-05 13:38:28 +0100

    My HADOOP-9576: Make NetUtils.wrapException throw EOFException instead of wrapping it as IOException

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/net/NetUtils.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/net/NetUtils.java
@@ -17,6 +17,7 @@
  */
 package org.apache.hadoop.net;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -756,11 +757,17 @@ public class NetUtils {
       return wrapWithMessage(exception,
           "No Route to Host from  "
               + localHost + " to " + destHost + ":" + destPort
-              + " failed on socket timeout exception: " + exception
+              + " failed on NoRouteToHostException : " + exception
               + ";"
               + see("NoRouteToHost"));
-    }
-    else {
+    } else if (exception instanceof EOFException) {
+      return wrapWithMessage(exception,
+          "EOFException from  "
+              + getHostDetailsAsString(destHost, destPort, localHost)
+              + exception
+              + ";"
+              + see("EOFException"));
+    } else {
       return (IOException) new IOException("Failed on local exception: "
                                                + exception
                                                + "; Host Details : "
