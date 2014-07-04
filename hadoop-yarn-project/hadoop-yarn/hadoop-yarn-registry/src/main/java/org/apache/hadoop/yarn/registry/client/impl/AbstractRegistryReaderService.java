74e06dfcd45492e1f4a2f66c16a872a8e7e7d1c3
Steve Loughran <stevel@apache.org>
2014-07-04 19:08:19 +0100

    YARN-913 stub out API struct and envisaged operations

---
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-registry/src/main/java/org/apache/hadoop/yarn/registry/client/impl/AbstractRegistryReaderService.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.yarn.registry.client.impl;
+
+import org.apache.hadoop.service.AbstractService;
+import org.apache.hadoop.yarn.registry.client.api.RegistryReader;
+import org.apache.hadoop.yarn.registry.client.api.ServiceEntry;
+
+import java.io.IOException;
+import java.util.List;
+
+public abstract class AbstractRegistryReaderService extends AbstractService implements
+    RegistryReader {
+
+  public AbstractRegistryReaderService(String name) {
+    super(name);
+  }
+
+  @Override
+  public List<String> getServiceClasses(String user) throws IOException {
+    return null;
+  }
+
+  @Override
+  public List<String> getServices(String user, String serviceClass) throws
+      IOException {
+    return null;
+  }
+
+  @Override
+  public ServiceEntry getServiceEntry(String user,
+      String serviceClass,
+      String name) throws IOException {
+    return null;
+  }
+}
