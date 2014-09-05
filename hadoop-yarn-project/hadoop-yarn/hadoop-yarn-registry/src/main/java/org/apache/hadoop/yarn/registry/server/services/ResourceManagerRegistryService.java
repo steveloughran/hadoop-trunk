7a11c79ebd5df1596cdee2f3047e32fa88c84246
Steve Loughran <stevel@apache.org>
2014-09-05 17:12:05 +0100

    YARN-913 purge entries

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-registry/src/main/java/org/apache/hadoop/yarn/registry/server/services/ResourceManagerRegistryService.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-registry/src/main/java/org/apache/hadoop/yarn/registry/server/services/ResourceManagerRegistryService.java
@@ -19,17 +19,30 @@
 package org.apache.hadoop.yarn.registry.server.services;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
+import org.apache.commons.lang.StringUtils;
+import org.apache.curator.framework.api.BackgroundCallback;
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.PathIsNotEmptyDirectoryException;
+import org.apache.hadoop.yarn.api.records.ApplicationAttemptId;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
 import org.apache.hadoop.yarn.registry.client.binding.BindingUtils;
+import org.apache.hadoop.yarn.registry.client.binding.RecordOperations;
 import org.apache.hadoop.yarn.registry.client.services.RegistryBindingSource;
 import org.apache.hadoop.yarn.registry.client.services.RegistryOperationsService;
+import org.apache.hadoop.yarn.registry.client.types.RegistryPathStatus;
+import org.apache.hadoop.yarn.registry.client.types.ServiceRecord;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.ACL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Extends the registry operations with extra support for resource management
@@ -39,7 +52,9 @@ import java.util.List;
 @InterfaceAudience.Private
 @InterfaceStability.Evolving
 public class ResourceManagerRegistryService extends RegistryOperationsService {
-
+  private static final Logger LOG =
+      LoggerFactory.getLogger(ResourceManagerRegistryService.class);
+  
   private List<ACL> rootRegistryACL;
 
   private List<ACL> userAcl;
@@ -95,12 +110,133 @@ public class ResourceManagerRegistryService extends RegistryOperationsService {
   public void createUserPath(String username) throws IOException {
     String path = BindingUtils.userPath(username);
     maybeCreate(path, CreateMode.PERSISTENT,
-        parseACLs(PERMISSIONS_REGISTRY_USER), false);
+        createAclForUser(username), false);
   }
 
 
+  /**
+   * Set up the ACL for the user.
+   * @param username user name
+   * @return an ACL list
+   * @throws IOException ACL creation/parsing problems
+   */
   private List<ACL> createAclForUser(String username) throws IOException {
-    return parseACLs(PERMISSIONS_REGISTRY_USER);
+    return userAcl;
   }
+
+
+  public void onApplicationAccepted() throws IOException {
+    
+  }
+
+  public void onApplicationLaunched() throws IOException {
+    
+  }
+
+  /**
+   * Actions to take as an AM registers itself with the RM. 
+   * @param user user owning the application
+   * @param attemptId attempt ID
+   * @throws IOException
+   */
+  public void onApplicationMasterRegistered(String user,
+      ApplicationAttemptId attemptId) throws IOException {
+    createUserPath(user);
+  }
+  
+  public void onContainerCompleted() throws IOException {
+    
+  }
+
+  public void onApplicationAttemptCompleted() throws IOException {
+    
+  }
+
+  public void onApplicationCompleted() throws IOException {
+    
+  }
+
+
+  /**
+   * Policy to purge entries
+   */
+  public enum PurgePolicy {
+    PurgeAll,
+    PurgeFailOnChildren,
+    PurgeSkipOnChildren
+  }
+  
+  /**
+   * Look under a base path and purge all records -recursively.
+   * <ol>
+   *   <li>Uses a depth first search</li>
+   *   <li>If a record matches then it is deleted without any child searches</li>
+   *   <li>Deletions will be asynchronous if a callback is provided</li>
+   * </ol>
+   * 
+   * @param basepath base path
+   * @param id ID for service record.id
+   * @param purgePolicy what to do if there is a matching record with children
+   * @throws IOException problems
+   * @throws PathIsNotEmptyDirectoryException if an attempt is made to 
+   */
+  @VisibleForTesting
+  public void purgeRecordsWithID(String basepath, String id,
+      PurgePolicy purgePolicy,
+      BackgroundCallback callback) throws IOException {
+    Preconditions.checkArgument(StringUtils.isNotEmpty(basepath),
+        "Empty 'basepath' argument");
+    Preconditions.checkArgument(StringUtils.isNotEmpty(id),
+        "Empty 'id' argument");
+    
+    // list this directory
+    RegistryPathStatus[] entries = listDir(basepath);
+    Map<String, RegistryPathStatus> subDirs =
+        new HashMap<String, RegistryPathStatus>(entries.length);
+    for (RegistryPathStatus status : entries) {
+      if (status.children> 0) {
+        subDirs.put(status.path, status);
+      }
+    }
+
+    Map<String, ServiceRecord> recordMap =
+        RecordOperations.extractServiceRecords(this, entries);
+
+    for (Map.Entry<String, ServiceRecord> recordEntry : recordMap.entrySet()) {
+      ServiceRecord serviceRecord = recordEntry.getValue();
+      String recordPath = recordEntry.getKey();
+      if (serviceRecord.id.equals(id)) {
+        // to purge
+        LOG.debug("Match for entry at {}", recordPath);
+        boolean toDelete = true;
+        // see if it has children, and act on that
+        if (subDirs.containsKey(recordPath)) {
+          // it does have children, so decide what to do
+          switch (purgePolicy) {
+            case PurgeSkipOnChildren:
+              // don't do the deletion... continue to next record
+              toDelete = false;
+              break;
+            case PurgeAll:
+              // mark for deletion
+              toDelete = true;
+              // remove the record from the subdir scan path as it is
+              // obsolete
+              subDirs.remove(recordPath);
+              break;
+            case PurgeFailOnChildren:
+              throw new PathIsNotEmptyDirectoryException(recordPath);
+          }
+        }
+        if (toDelete) {
+          zkDelete(recordPath, true, callback);
+        }
+      }
+    }
+    
+    // now go through the children
+
+  }
+
 
 }
